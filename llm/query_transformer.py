3b9381829a805230448a19bd384fa367d65f1460
pdrm83 <pedram.ataee@gmail.com>
2024-10-25 22:33:22 +0330

    Update

---
+++ llm/query_transformer.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer
+from auto_gptq import AutoModelForCausalLM
+
+model_name_or_path = "TheBloke/Llama-2-13B-chat-GPTQ"
+# To use a different branch, change revision
+# For example: revision="main"
+model = AutoModelForCausalLM.from_pretrained(
+    model_name_or_path,
+    device_map="auto",
+    trust_remote_code=False,
+    revision="main",
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+prompt = "Tell me about AI"
+prompt_template = f"""[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully
+as possible, while being safe.  Your answers should not include any harmful,
+unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure
+that your responses are socially unbiased and positive in nature. If a question
+does not make any sense, or is not factually coherent, explain why instead of
+answering something not correct. If you don't know the answer to a question,
+please don't share false information.
+<</SYS>>
+{prompt}[/INST]
+
+"""
+print("\n\n*** Generate:")
+
+input_ids = tokenizer(prompt_template, return_tensors="pt").input_ids.cuda()
+output = model.generate(
+    inputs=input_ids,
+    temperature=0.7,
+    do_sample=True,
+    top_p=0.95,
+    top_k=40,
+    max_new_tokens=512,
+)
+print(tokenizer.decode(output[0]))
